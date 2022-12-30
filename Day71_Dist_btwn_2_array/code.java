1b77317cd0ed14cc08226c82bc8a669ebbddbf15
Ayushi-Khare <56400384+Ayushi-Khare@users.noreply.github.com>
2022-12-31 02:28:13 +0530

    Create code.java

---
+++ Day71_Dist_btwn_2_array/code.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) 
+    {
+        int count=0;
+        for(int i=0; i<arr1.length; i++)
+        {
+            int flag=0;
+            for(int j=0; j<arr2.length; j++)
+            {
+                int x=arr1[i]-arr2[j];
+                if(x<0)
+                {
+                    x=x*(-1);
+                }
+
+                if(x<=d)
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                count++;
+            }
+        }
+        return(count);   
+    }
+}
